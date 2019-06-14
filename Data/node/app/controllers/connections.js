e1b1a801d67b841e611e712154ec34233d746e0e
yindaheng98 <yindaheng98@163.com>
2019-06-15 02:41:37 +0800

    云端大改，基本完成

--- Data/node/app/controllers/connections.js
+++ Data/node/app/controllers/connections.js
@@ -3,14 +3,14 @@ let redis = require('redis');
 
 function Connection() {
     this.mysql = mysql.createConnection({
-            host: 'localhost',
+            host: 'mysql',
             user: 'CarsData',
             password: 'CarsData',
             port: '3306',
             database: 'CarsData'
         });
     this.mysql.connect();
-    this.redis = redis.createClient(6379,'localhost');
+    this.redis = redis.createClient(6379,'redis');
 }
 
 module.exports = new Connection();
