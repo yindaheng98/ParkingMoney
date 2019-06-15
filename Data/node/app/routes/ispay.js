b6e57a58a9bf933709c8351b68d79d0b634d9078
yindaheng98 <yindaheng98@163.com>
2019-06-15 17:27:57 +0800

    云端小改，基本完成

--- Data/node/app/routes/ispay.js
+++ Data/node/app/routes/ispay.js
@@ -2,12 +2,26 @@ let express = require('express');
 let router = express.Router();
 const con = require('../controllers/connections');
 
+function isPay(id, during, times) {
+    return new Promise((resolve, reject) => {
+        (function queryLoop(during, times) {
+            if (times <= 0) return reject('超时');
+            con.redis.get(id, function (err, value) {//先查一遍
+                if (err !== null) return reject(err);
+                if (value === null) return resolve();
+                setTimeout(() => queryLoop(during, times - 1), during);
+            });
+        })(during, times);
+    })
+}
+
 router.get('/:id', function (request, response, next) {
-    let id = request.params.id;
-    con.redis.get(id, function (err, value) {//先查一遍
-        if (err !== null) return response.end('error');
-        return response.end(value === null ? 'yes' : 'no');
-    });
+    isPay(request.params.id, 500, 10000)
+        .then(() => response.end('yes'))
+        .catch((e) => {
+            response.end('error');
+            console.log(e);
+        })
 });
 
 module.exports = router;
