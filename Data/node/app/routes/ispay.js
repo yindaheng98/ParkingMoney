59637cb28577ef571b508a60aa96f67d1638289d
yindaheng98 <yindaheng98@163.com>
2019-07-04 12:28:55 +0800

    用async-await重写了查询逻辑

--- Data/node/app/routes/ispay.js
+++ Data/node/app/routes/ispay.js
@@ -5,7 +5,7 @@ const con = require('../controllers/connections');
 function isPay(id, during, times) {
     return new Promise((resolve, reject) => {
         (function queryLoop(during, times) {
-            if (times <= 0) return reject('timeout');
+            if (times <= 0) return resolve('timeout');
             con.redis.get(id, (err, value) => {//先查一遍
                 if (err !== null) return reject(err);
                 if (value === null) return resolve();
@@ -15,15 +15,11 @@ function isPay(id, during, times) {
     })
 }
 
-router.get('/:id', (request, response, next) => {
-    isPay(request.params.id, 500, 10000)
-        .then(() => response.end('yes'))
-        .catch((e) => {
-            if (e === 'timeout')
-                response.end('no');
-            response.end('error');
-            console.log(e);
-        })
+router.get('/:id', async (request, response, next) => {
+    let result = isPay(request.params.id, 500, 10000);
+    if (result === 'yes') return response.end('yes');
+    response.end('error');
+    console.log("来自树莓派请求，订单查询结果为" + result);
 });
 
 module.exports = router;
